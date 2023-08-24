f40bc0f851995bfafa42679238d4ebf42463b1f4
Keshav Kumar <keshavkumar8122@gmail.com>
2023-08-24 20:37:11 +0530

    React Counter

--- 01-react-counter/src/App.jsx
+++ 01-react-counter/src/App.jsx
@@ -4,16 +4,28 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  const increment = () => {
+    setCount(count + 1)
+  }
+
+  const decrement = () => {
+    setCount(count - 1)
+  }
+
   return (
     <>
-      <h1>Vite + React</h1>
+      <h1>Counter App 101 </h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <h2>Count is {count}</h2>
+        <br></br>
+        <button onClick={increment}>Increment</button>
+        <br></br>
+        <br></br>
+        <button onClick={decrement}>Decrement</button>
       </div>
     </>
   )
 }
+
 
 export default App
